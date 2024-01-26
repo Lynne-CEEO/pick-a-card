59e00d932cc6f8419b2cb3b941f9f8dffcb66ea4
Lynne-CEEO <146778096+Lynne-CEEO@users.noreply.github.com>
2024-01-26 11:34:58 -0500

    Create pick.js
    clone

---
+++ pick.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $.get("https://deckofcardsapi.com/api/deck/new/draw/?count=1", function (
+    data
+  ) {
+    const suit = data.cards[0].suit;
+    const value = data.cards[0].value;
+    const image = data.cards[0].image;
+    document.getElementById("suit").innerHTML = suit;
+    document.getElementById("value").innerHTML = value;
+    document.getElementById("image").src = image;
+  });
+});
+
+function newCard() {
+  location.reload();
+}
